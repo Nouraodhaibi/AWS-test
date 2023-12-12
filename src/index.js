a9ca81371874cf55522009e63568fe4a39671f34
nouraodhaibi <nouraodhaibi.com>
2023-12-12 05:04:17 +0300

    5

--- src/index.js
+++ src/index.js
@@ -2,19 +2,29 @@ const express = require("express");
 
 const app = express();
 const port =3005;
+let products = [
+     {id: 1, title: "iphone 15", price: 480},
+            {id: 2, title: "iphone 16", price: 500},
+            {id: 3, title: "iphone 17", price: 600},
+            {id: 4, title: "iphone 18", price: 700},
+]
+
 app.get("/", (req,res)=>{
     res.send("welcome to express server")
 
 });
 app.get("/products", (req,res)=>{
     res.send({
-        products : [
-            {id: 1, title: "iphone 15", price: 480},
-            {id: 2, title: "iphone 16", price: 500},
-            {id: 3, title: "iphone 17", price: 600},
-            {id: 4, title: "iphone 18", price: 700},
-        ]
+        products : products,
     })
+
+});
+app.get("/products/:id", (req,res)=>{
+   const id = Number(req.params.id);
+   const product = products.find((product)=> product.id === id);
+res.send({
+    product: product,
+});
 
 });
 app.listen(port,() => {
